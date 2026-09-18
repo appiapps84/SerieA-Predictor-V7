e85ac49fd6fbb0bc3cb30f943f7b661eb9abc559
appiapps84 <appiapps84@gmail.com>
2026-09-19 00:06:05 +0200

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -234,9 +234,29 @@ function lambdaFromUnderstat(homeU, awayU) {
   const awayAttack = awayXgFor / LEAGUE_AVG_XG;
   const homeDefense = homeXgAgainst / LEAGUE_AVG_XG;
 
+   const lambdaHome = LEAGUE_HOME_XG * homeAttack * awayDefense;
+  const lambdaAway = LEAGUE_AWAY_XG * awayAttack * homeDefense;
+
+  console.log("UNDERSTAT_DEBUG", JSON.stringify({
+    homePlayed,
+    awayPlayed,
+    K,
+    homeRaw: homeU.xgForPerGame,
+    homeShrunk: homeXgFor,
+    homeAgainstShrunk: homeXgAgainst,
+    awayRaw: awayU.xgForPerGame,
+    awayShrunk: awayXgFor,
+    awayAgainstShrunk: awayXgAgainst,
+    homeAttack, awayDefense,
+    awayAttack, homeDefense,
+    lambdaHome, lambdaAway
+  }));
+
   return {
-    home: LEAGUE_HOME_XG * homeAttack * awayDefense,
-    away: LEAGUE_AWAY_XG * awayAttack * homeDefense
+    home: lambdaHome,
+    away: lambdaAway
+  };
+}
   };
 }
 function lambdaFromForm(homeForm, awayForm) {
